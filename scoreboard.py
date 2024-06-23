991e02141f275355e308f340356b701bcf1d156b
Aayush <aayush.byanjankar28@gmail.com>
2024-06-23 10:16:49 -0500

    new update

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,15 @@
 from turtle import Turtle
 from food import Food
 
+with open("data.txt") as file:
+    point = file.read()
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = int(point)
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -13,13 +17,21 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} Highscore: {self.highscore}", align="center", font=("Arial", 24, "normal"))
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GameOver!", align="center", font=("Arial", 24, "normal"))
+    def reset_score(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GameOver!", align="center", font=("Arial", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
